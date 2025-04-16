e238243b6457dd897db308bc1e1ac55e539d6e5c
userWaliso <walisson.santana167@gmail.com>
2025-04-15 21:46:39 -0300

    Update C2.go

--- PDFs/C2.go
+++ PDFs/C2.go
@@ -20,3 +20,26 @@ func main() {
 		fmt.Printf("%d\n%d\n",soma, media)
 	}
 }
+
+package main
+import (
+	f"fmt"
+)
+func buscabinaria() {
+	var x
+	array := make([10], int) {2, 3, 7, 12, 15, 17, 23, 30, 40, 50}
+
+	e := 0
+	d := len[array] - 1
+
+	for e <= D {
+		m:= (e + d) / 2
+		if len[array] == x
+			return m
+		if len[array] < x
+			e = m + 1
+		if len[array] > x
+			d = m - 1
+	} return -1
+
+}
